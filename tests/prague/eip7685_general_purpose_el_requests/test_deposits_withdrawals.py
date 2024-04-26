c3bb752db498ea92a6d5bfc75b3947df3e5da16b
Mario Vega <marioevz@gmail.com>
2024-04-26 21:26:39 +0000

    new(tests): eip-7685: add tests

---
+++ tests/prague/eip7685_general_purpose_el_requests/test_deposits_withdrawals.py
@@ -0,0 +1,196 @@
+"""
+abstract: Tests [EIP-7685: General purpose execution layer requests](https://eips.ethereum.org/EIPS/eip-7685)
+    Cross testing for withdrawal and deposit request for [EIP-7685: General purpose execution layer requests](https://eips.ethereum.org/EIPS/eip-7685)
+
+"""  # noqa: E501
+
+from dataclasses import dataclass
+from typing import Dict, List
+
+import pytest
+
+from ethereum_test_tools import (
+    Account,
+    Address,
+    Block,
+    BlockchainTestFiller,
+    BlockException,
+    DepositRequest,
+    Environment,
+    Header,
+    TestAddress,
+    TestAddress2,
+    TestPrivateKey,
+    TestPrivateKey2,
+    Transaction,
+    WithdrawalRequest,
+)
+
+from ..eip6110_deposits.test_deposits import (
+    DepositContract,
+    DepositTransaction,
+    DepositTransactionBase,
+)
+from ..eip7002_el_triggerable_withdrawals.test_withdrawal_requests import (
+    WithdrawalRequestContract,
+    WithdrawalRequestTransaction,
+    WithdrawalRequestTransactionBase,
+)
+from .spec import ref_spec_7685
+
+REFERENCE_SPEC_GIT_PATH = ref_spec_7685.git_path
+REFERENCE_SPEC_VERSION = ref_spec_7685.version
+
+pytestmark = pytest.mark.valid_from("Prague")
+
+
+@dataclass
+class SenderAccount:
+    """Test sender account descriptor."""
+
+    address: Address
+    key: str
+
+
+TestAccount1 = SenderAccount(TestAddress, TestPrivateKey)
+TestAccount2 = SenderAccount(TestAddress2, TestPrivateKey2)
+
+##############
+#  Fixtures  #
+##############
+
+
+@pytest.fixture
+def pre(
+    requests: List[DepositTransactionBase | WithdrawalRequestTransactionBase],
+) -> Dict[Address, Account]:
+    """
+    Initial state of the accounts. Every deposit transaction defines their own pre-state
+    requirements, and this fixture aggregates them all.
+    """
+    pre = {}
+    for d in requests:
+        pre.update(d.pre())
+    return pre
+
+
+@pytest.fixture
+def txs(
+    requests: List[DepositTransactionBase | WithdrawalRequestTransactionBase],
+) -> List[Transaction]:
+    """List of transactions to include in the block."""
+    return [d.transaction() for d in requests]
+
+
+@pytest.fixture
+def block_requests() -> List[DepositRequest] | None:
+    """List of requests that overwrite the requests in the header. None by default."""
+    return None
+
+
+@pytest.fixture
+def exception() -> BlockException | None:
+    """Block exception expected by the tests. None by default."""
+    return None
+
+
+@pytest.fixture
+def blocks(
+    requests: List[DepositTransactionBase | WithdrawalRequestTransactionBase],
+    block_requests: List[DepositRequest | WithdrawalRequest] | None,
+    txs: List[Transaction],
+    exception: BlockException | None,
+) -> List[Block]:
+    """List of blocks that comprise the test."""
+    included_deposit_requests = []
+    included_withdrawal_requests = []
+    # Single block therefore base fee
+    withdrawal_request_fee = 1
+    for r in requests:
+        if isinstance(r, DepositTransactionBase):
+            included_deposit_requests += r.included_deposits()
+        elif isinstance(r, WithdrawalRequestTransactionBase):
+            included_withdrawal_requests += r.valid_withdrawal_requests(withdrawal_request_fee)
+
+    return [
+        Block(
+            txs=txs,
+            header_verify=Header(
+                requests_root=included_deposit_requests + included_withdrawal_requests,
+                requests=block_requests,
+                exception=exception,
+            ),
+        )
+    ]
+
+
+################
+#  Test cases  #
+################
+
+
+@pytest.mark.parametrize(
+    "requests",
+    [
+        pytest.param(
+            [
+                DepositTransaction(
+                    deposit_request=DepositRequest(
+                        pubkey=0x01,
+                        withdrawal_credentials=0x02,
+                        amount=32_000_000_000,
+                        signature=0x03,
+                        index=0x0,
+                    ),
+                    nonce=0,
+                ),
+                WithdrawalRequestTransaction(
+                    withdrawal_request=WithdrawalRequest(
+                        validator_public_key=0x01,
+                        amount=0,
+                    ),
+                    fee=1,
+                    nonce=1,
+                ),
+            ],
+            id="single_deposit_from_eoa_single_withdrawal_from_eoa",
+        ),
+        pytest.param(
+            [
+                WithdrawalRequestTransaction(
+                    withdrawal_request=WithdrawalRequest(
+                        validator_public_key=0x01,
+                        amount=0,
+                    ),
+                    fee=1,
+                    nonce=0,
+                ),
+                DepositTransaction(
+                    deposit_request=DepositRequest(
+                        pubkey=0x01,
+                        withdrawal_credentials=0x02,
+                        amount=32_000_000_000,
+                        signature=0x03,
+                        index=0x0,
+                    ),
+                    nonce=1,
+                ),
+            ],
+            id="single_withdrawal_from_eoa_single_deposit_from_eoa",
+        ),
+    ],
+)
+def test_valid_deposit_withdrawal_requests(
+    blockchain_test: BlockchainTestFiller,
+    pre: Dict[Address, Account],
+    blocks: List[Block],
+):
+    """
+    Test making a deposit to the beacon chain deposit contract.
+    """
+    blockchain_test(
+        genesis_environment=Environment(),
+        pre=pre,
+        post={},
+        blocks=blocks,
+    )
